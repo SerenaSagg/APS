e2ec3750bc9065ff7f4d67625862f4a5dee1bcab
Sere <sere2311@hotmail.com>
2026-09-04 13:12:32 -0300

    Actualizado

--- TS1.py
+++ TS1.py
@@ -7,6 +7,8 @@ Created on Fri Mar 27 17:31:02 2026
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import signal
+
 
 def mi_sen(vmax, dc, ff, ph, nn, fs):
     Ts = 1/fs
@@ -14,120 +16,74 @@ def mi_sen(vmax, dc, ff, ph, nn, fs):
     xx = dc + vmax * np.sin(2*np.pi*ff*tt + ph)
     return tt, xx
 
-def cuadrada(A, f, nn, fs):
-    tt = np.arange(nn) / fs
-    xx = A * np.sign(np.sin(2*np.pi*f*tt))
-    return tt, xx
-
-def escalon(A, t0, nn, fs):
-    tt = np.arange(nn) / fs
-    xx = np.zeros(nn)
-    xx[tt >= t0] = A
-    return tt, xx
-
-def pulso_rect(A, t0, tf, nn, fs):
-    tt, xu1 = escalon(A, t0, nn, fs)
-    tt, xu2 = escalon(A, tf, nn, fs)
-    xc = xu1 - xu2
-    return tt, xc
-
 # Parámetros
-fs = 2000001
-N = 10000
+Np = 10
+fs = 20000
+ff = fs/Np
+N = 1000
+T = 1/ff
 
 # 1) Señal original de 2 kHz
-tt1, x1 = mi_sen(vmax=1, dc=0, ff=2000, ph=0, nn=N, fs=fs)
+tt1, x1 = mi_sen(vmax=1, dc=0, ff=ff, ph=0, nn=N, fs=fs)
 P1=np.var(x1)
-# 2) Misma señal amplificada +3 dB y desfasada en pi/2
-fs2 = 2000001
-N2 = 10000
-tt2, x2 = mi_sen(vmax=np.sqrt(2), dc=0, ff=2000, ph=np.pi/2, nn=N2, fs=fs2)
+
+# 2) Misma señal con potencia 2W y desfasada en pi/2
+Pot=2
+vmax=np.sqrt(Pot*2)
+tt2, x2 = mi_sen(vmax=vmax, dc=0, ff=ff, ph=np.pi/2, nn=N, fs=fs)
 P2=np.var(x2)
 
-# 3) Modulación
-# Parámetros
-fs3 = 10000000
-N3 = 100000
+tt_ruido = np.arange(N) / fs
+# 3) Ruido aleatorio con distribucion normal
+Var=0.1
+nn1=np.random.normal(0,np.sqrt(Var),N)  # Ruido gaussiano de media 0 y varianza 0.1
 
-#Señal original
-tt3, xx1 = mi_sen(vmax=1, dc=0, ff=2000, ph=0, nn=N3, fs=fs3)
-#Portadora
-tt3, xm = mi_sen(vmax=1, dc=0, ff=1000000, ph=0, nn=N3, fs=fs3)
-# Señal modulada
-x3 = (1 + xx1) * xm
+# 4) Ruido aleatorio con distribucion uniforme
+Var=0.1
+nn2=np.random.uniform(0,np.sqrt(Var),N)  # Ruido gaussiano de media 0 y varianza 0.1
 
+# 5) Pulso rectangular
+tt3 = np.arange(N) / fs
 
-# 4) Señal saturada al 75%
-fs4 = 2000001
-N4 = 10000
-tt4, x4 = mi_sen(vmax=1, dc=0, ff=2000, ph=0, nn=N4, fs=fs4)
-x4 = np.clip(x4, -0.75, 0.75)
-
-
-# 5) Señal cuadrada
-fs5 = 2000001
-N5 = 10000
-tt5,x5=cuadrada(A=1, f=4000, nn=N5, fs=fs5)
-
-# 6) Pulso rectangular
-fs6 = 2000001
-N6 = 40000
-tt6, x6 = pulso_rect(A=1, t0=0.002, tf=0.012, nn=N6, fs=fs6)
-
+x3 = signal.square(2*np.pi*ff*tt3,duty=0.5)
 
 # Gráfico 1
 plt.figure()
-plt.plot(tt1, x1, label="f = 2 kHz")
+plt.plot(tt1, x1)
 plt.title("Señal senoidal")
 plt.xlabel("Tiempo [s]")
 plt.ylabel("Amplitud [V]")
+plt.xlim(0, 0.01)
 plt.legend()
 plt.grid()
 plt.show()
 
 # Gráfico 2
 plt.figure()
-plt.plot(tt2, x2, label="Amplificada +3 dB y desfasada")
-plt.title("Señal amplificada")
+plt.plot(tt2, x2)
+plt.title("Señal amplificada y desfasada")
 plt.xlabel("Tiempo [s]")
 plt.ylabel("Amplitud [V]")
+plt.xlim(0, 0.01)
 plt.legend()
 plt.grid()
 plt.show()
 
 # Gráfico 3
-plt.figure(figsize=(12,12))
-plt.subplot(3, 1,1)
-plt.title("Modulación")
-plt.plot(tt3, xx1, label="Senoidal", color="lime")
+plt.figure()
+plt.title("Ruido Aleatotio con distribución Normal")
+plt.plot(tt_ruido,nn1, color="lime")
 plt.xlabel("Tiempo [s]")
 plt.ylabel("Amplitud [V]")
 plt.legend()
 plt.grid()
 
-plt.subplot(3, 1,2)
-plt.plot(tt3,xm, label="Señal Portadora", color="aqua")
-plt.xlim(0, 0.00001) #zoom
-plt.xlabel("Tiempo [s]")
-plt.ylabel("Amplitud [V]")
-plt.legend()
-plt.grid()
 
-plt.subplot(3, 1,3)
-plt.plot(tt3,x3, label="Señal Modulada", color="fuchsia")
-plt.plot(tt3,(1+xx1), 'b--', label="Señal Portadora")
-plt.plot(tt3,-(1+xx1), 'b--')
-plt.xlim(0, 0.002) #zoom
-plt.xlabel("Tiempo [s]")
-plt.ylabel("Amplitud [V]")
-plt.legend()
-plt.grid()
-plt.show()
 
 # Gráfico 4
 plt.figure()
-plt.plot(tt4, x4, label="Saturada al 75%")
-plt.title("Señal saturada")
+plt.plot(tt_ruido, nn2)
+plt.title("Ruido Aleatotio con distribución Uniforme")
 plt.xlabel("Tiempo [s]")
 plt.ylabel("Amplitud [V]")
 plt.legend()
@@ -136,23 +92,15 @@ plt.show()
 
 # Gráfico 5
 plt.figure()
-plt.plot(tt5, x5, label="f = 4KHz")
-plt.title("Señal cuadrada")
+plt.plot(tt3, x3)
+plt.title("Pulso rectangular")
 plt.xlabel("Tiempo [s]")
 plt.ylabel("Amplitud [V]")
+plt.xlim(0, 0.003) 
 plt.legend()
 plt.grid()
 plt.show()
 
-# Gráfico 6
-plt.figure()
-plt.plot(tt6, x6, label="t=10 ms", color="lime")
-plt.title("Pulso rectangular")
-plt.xlabel("Tiempo [s]")
-plt.ylabel("Amplitud [V]")
-plt.legend()
-plt.grid()
-plt.show()
 
 # Señal de energía: tiene energía finita y potencia media 0.
 # Señal de potencia: tiene energía infinita pero potencia media finita y no nula.
